240acbcfdaef4ffa4bcc4161e27b5342684a49f9
Charlie Brooking <cbrookin@thoughtworks.com>
2018-11-18 23:29:26 +1000

    cleanup invalid e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,7 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to example!');
   });
 });
